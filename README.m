aa440ec7294321263dceaef2bcbd525e98ebf8ba
raviyazareen22 <132916138+raviya9603731342@users.noreply.github.com>
2023-05-16 16:46:54 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,2 +1,4 @@
 # internship_training
-<p align="justify"> i am raviya here representing you </p>
+<p align="justify"> i am raviya here representing you here as a student and
+here i am gonna learn about vlsi and its applications 
+</p>
