b44bf7e1d0f4902649df8e1094833c9e075f7f85
Raviya zareen <132916138+Raviyazareen22@users.noreply.github.com>
2023-05-18 18:43:04 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,4 +1,6 @@
 # internship_training
-<p align="justify"> i am raviya here representing you here as a student and
+<p align="justify"> i am raviya here representing you as a student and
 here i am gonna learn about vlsi and its applications 
+IC Design or Integrated Circuit Design this course requires the highly skilled professionals to design electronic chips also called ICs.
+  Broadly IC Design approaches are of two types 1. ASICs (Application Specific Integrated Circuit) 2. FPGAs (Field Programmable Gate Array).
 </p>
